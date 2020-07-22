4166e7112a623a12b007b9fdec017f8689a582d8
abun880007 <abun880007@msn.com>
2020-07-22 11:55:40 +0100

    420rom time

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all exynos products
-# that inherit from Lineage
+# that inherit from 420rom
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,14 +10,14 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/exynos/private
+    device/420rom/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/420rom/sepolicy/exynos/dynamic \
+    device/420rom/sepolicy/exynos/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/420rom/sepolicy/exynos/dynamic \
+    device/420rom/sepolicy/exynos/vendor
 endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from 420rom
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,19 +10,19 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/420rom/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/system
+    device/420rom/sepolicy/qcom/dynamic \
+    device/420rom/sepolicy/qcom/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/420rom/sepolicy/qcom/dynamic \
+    device/420rom/sepolicy/qcom/vendor
 endif
 
 ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/legacy-vendor
+    device/420rom/sepolicy/qcom/legacy-vendor
 endif
